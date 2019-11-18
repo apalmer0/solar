82311c11b2ca9b0cf653f5ee51dc86564c51e1c4
Andrew Palmer <andrew.palmer.10@gmail.com>
2019-11-18 08:59:57 -0500

    Additional refactoring, removing unneeded code

--- lib/solar/position.rb
+++ lib/solar/position.rb
@@ -41,7 +41,6 @@ module Solar
     attr_reader :datetime, :latitude, :longitude
 
     def hour_angle(right_ascension)
-      # Local hour angle
       hour_angle = sidereal_time + longitude - right_ascension
       hour_angle %= 360
     end
@@ -49,11 +48,11 @@ module Solar
     def sidereal_time
       julian_day = julian_day_rational(datetime)
       # datetime as Julian centuries of 36525 ephemeris days form the epoch J2000.0
-      datetime_in_centuries = to_julian_centuries(datetime)
-      # Sidereal time at Greenwich
-      time = 280.46061837 + 360.98564736629 * julian_days_since_2000(julian_day) + (0.000387933 - datetime_in_centuries/38_710_000)*datetime_in_centuries*datetime_in_centuries
+      julian_centuries = to_julian_centuries(datetime)
+
+      time_at_greenwich = 280.46061837 + 360.98564736629 * julian_days_since_2000(julian_day) + (0.000387933 - julian_centuries/38_710_000)*julian_centuries*julian_centuries
       # Reduce magnitude to minimize errors
-      time %= 360
+      time_at_greenwich %= 360
     end
 
     def altitude_rad(latitude_rad, declination, hour_angle_rad)
--- lib/solar/support.rb
+++ lib/solar/support.rb
@@ -25,7 +25,8 @@ module Support
 
   # Julian day to Julian Centuries since J2000.0
   def to_julian_centuries(t)
-    julian_day = julian_day_rational(to_dynamical_time(t))
+    julian_day = julian_day_rational(t)
+
     julian_days_since_2000(julian_day) / DAYS_PER_CENTURY
   end
 
@@ -33,117 +34,22 @@ module Support
     julian_day - JULIAN_DAY_NUMBER_FOR_2000
   end
 
-  def polynomial(coefficients, x)
-    coefficients.inject(0.0) { |p, a| p * x + a }
-  end
-
-  # Conversion of Float to Rational preserving the exact value of the number
-  def to_rational(x)
-    x = x.to_f
-    return Rational(x.to_i,1) if x.modulo(1) == 0
-
-    if !x.finite?
-      return Rational(0,0) if x.nan?
-      return x < 0 ? Rational(-1,0) : Rational(1,0)
-    end
-
-    f,e = Math.frexp(x)
-
-    if e < Float::MIN_EXP
-      bits = e + Float::MANT_DIG - Float::MIN_EXP
-    else
-      bits = [Float::MANT_DIG, e].max
-    end
-    p = Math.ldexp(f,bits)
-    e = bits - e
-
-    if e<Float::MAX_EXP
-      q = Math.ldexp(1, e)
-    else
-      q = Float::RADIX**e
-    end
-
-    Rational(p.to_i, q.to_i)
-  end
-
-  # time to dynamical time
-  def to_dynamical_time(datetime)
-    datetime = datetime.utc
-
-    datetime + to_rational(delta_t(datetime)) / SECONDS_PER_DAY
-  end
-
-  # Compute difference between dynamical time and UTC in seconds.
-  # See http://sunearth.gsfc.nasa.gov/eclipse/SEcat5/deltatpoly.html.
-  # Good from -1999 to +3000.
-  def delta_t(date)
-    year = date.year.to_f
-    y = year + (date.month.to_f - 0.5) / 12.0
-
-    case
-    when year < -500.0
-      u = (year - 1820.0) / 100.0
-      -20.0 + 32.0*u*u
-    when year < 500.0
-      u = y / 100.0
-      polynomial [0.0090316521, 0.022174192, -0.1798452, -5.952053, 33.78311, -1014.41, 10583.6], u
-    when year < 1600.0
-      u = (y - 1000.0) / 100.0
-      polynomial [0.0083572073, -0.005050998, -0.8503463, 0.319781, 71.23472, -556.01, 1574.2], u
-    when year < 1700.0
-      t = y - 1600.0
-      polynomial [1.0/7129.0, -0.01532, -0.9808, 120.0], t
-    when year < 1800.0
-      t = y - 1700.0
-      polynomial [-1.0/1174000.0, 0.00013336, -0.0059285, 0.1603, 8.83], t
-    when year < 1860.0
-      t = y - 1800.0
-      polynomial [0.000000000875, -0.0000001699, 0.0000121272, -0.00037436, 0.0041116, 0.0068612, -0.332447, 13.72], t
-    when year < 1900.0
-      t = y - 1860.0
-      polynomial [1.0/233174.0, -0.0004473624, 0.01680668, -0.251754, 0.5737, 7.62], t
-    when year < 1920.0
-      t = y - 1900.0
-      polynomial [-0.000197, 0.0061966, -0.0598939, 1.494119, -2.79], t
-    when year < 1941.0
-      t = y - 1920.0
-      polynomial [0.0020936, -0.076100, 0.84493, 21.20], t
-    when year < 1961.0
-      t = y - 1950.0
-      polynomial [1.0/2547.0, -1.0/233.0, 0.407, 29.07], t
-    when year < 1986.0
-      t = y - 1975.0
-      polynomial [-1.0/718.0, -1.0/260.0, 1.067, 45.45], t
-    when year < 2005.0
-      t = y - 2000.0
-      polynomial [0.00002373599, 0.000651814, 0.0017275, -0.060374, 0.3345, 63.86], t
-    when year < 2050.0
-      t = y - 2000.0
-      polynomial [0.005589, 0.32217, 62.92], t
-    when year < 2150.0
-      -20.0 + 32.0*((y - 1820.0)/100.0)**2 - 0.5628*(2150.0 - y)
-    else
-      u = (year - 1820.0) / 100.0
-      -20.0 + 32*u*u
-    end
-  end
-
   # Solar equatorial coordinates / Low accuracy : Meeus pg 151
   # returns [declination in radians, right ascension in radians]
-  def equatorial_position(t)
+  def equatorial_position(datetime)
     # t as Julian centuries of 36525 ephemeris days form the epoch J2000.0
-    t = to_julian_centuries(t)
+    julian_centuries = to_julian_centuries(datetime)
 
     # Geometric mean longitude of the Sun, referred to the mean equinox of the date
-    l = 280.46645 + (36_000.76983 + 0.0003032*t)*t
+    l = 280.46645 + (36_000.76983 + 0.0003032*julian_centuries)*julian_centuries
 
     # Mean anomaly of the Sun
-    m_deg = 357.52910 + (35_999.05030 - (0.0001559 + 0.00000048*t)*t)*t
+    m_deg = 357.52910 + (35_999.05030 - (0.0001559 + 0.00000048*julian_centuries)*julian_centuries)*julian_centuries
     m_rad = to_radians(m_deg)
 
     # Sun's Equation of the center
-    c = (1.914600 - (0.004817 + 0.000014*t)*t)*Math.sin(m_rad) +
-    (0.019993 - 0.000101*t)*Math.sin(2*m_rad) +
+    c = (1.914600 - (0.004817 + 0.000014*julian_centuries)*julian_centuries)*Math.sin(m_rad) +
+    (0.019993 - 0.000101*julian_centuries)*Math.sin(2*m_rad) +
     0.000290*Math.sin(3*m_rad)
 
     # Sun's true longitude
@@ -153,7 +59,7 @@ module Support
     o %= 360
 
     # Sun's apparent Longitude
-    omega_deg = 125.04 - 1934.136*t
+    omega_deg = 125.04 - 1934.136*julian_centuries
     omega_rad = to_radians(omega_deg)
     lambda_deg = o - 0.00569 - 0.00478 * Math.sin(omega_rad)
 
@@ -164,7 +70,7 @@ module Support
 
     # Obliquity of the ecliptic
     epsilon_deg = 23.4392966666667 -
-    (0.012777777777777778 + (0.00059/60 - 0.00059/60*t)*t)*t +
+    (0.012777777777777778 + (0.00059/60 - 0.00059/60*julian_centuries)*julian_centuries)*julian_centuries +
     0.00256*Math.cos(omega_rad)
     epsilon_rad = to_radians(epsilon_deg)
 
